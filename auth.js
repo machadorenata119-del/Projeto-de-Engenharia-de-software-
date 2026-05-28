f0e98ba4ff75c3c69dac21b6e0237a6e59e80be9
machadorenata119-del <machadorenata119@gmail.com>
2026-05-28 15:25:59 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,44 +1,47 @@
-function mostrarSenha() {
+function mostrarSenha(){
+
   const senha = document.getElementById("senha");
 
-  if (senha.type === "password") {
+  if(senha.type === "password"){
     senha.type = "text";
-  } else {
+  }else{
     senha.type = "password";
   }
+
 }
 
-function cadastrar() {
-  const nome = document.getElementById("nome").value;
-  const email = document.getElementById("novoEmail").value;
-  const senha = document.getElementById("novaSenha").value;
+function login(){
 
-  const usuario = {
-    nome,
-    email,
-    senha
-  };
-
-  localStorage.setItem("usuario", JSON.stringify(usuario));
-
-  alert("Conta criada com sucesso!");
-  window.location.href = "login.html";
-}
-
-function login() {
   const email = document.getElementById("email").value;
+
   const senha = document.getElementById("senha").value;
 
   const usuario = JSON.parse(localStorage.getItem("usuario"));
 
-  if(email === usuario.email && senha === usuario.senha){
-    alert("Login realizado!");
+  if(
+    usuario &&
+    email === usuario.email &&
+    senha === usuario.senha
+  ){
+
+    localStorage.setItem("logado","true");
+
+    alert("Login realizado com sucesso!");
+
     window.location.href = "index.html";
-  } else {
+
+  }else{
+
     alert("Email ou senha incorretos!");
+
   }
+
 }
 
 function logout(){
+
+  localStorage.removeItem("logado");
+
   window.location.href = "login.html";
+
 }
