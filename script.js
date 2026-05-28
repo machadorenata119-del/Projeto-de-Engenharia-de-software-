d24ee437b2cc713651af0cab4980664d3a03afc2
machadorenata119-del <machadorenata119@gmail.com>
2026-05-28 10:28:19 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const adotar = document.querySelectorAll(".adotar");
+
+adotar.forEach(botao => {
+
+    botao.addEventListener("click", () => {
+
+        alert("🐾 Solicitação de adoção enviada com sucesso!");
+
+    });
+
+});
